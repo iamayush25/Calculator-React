df1476a196e81ed84730a5f4fc228a0380b3f5d3
Ayush Sharma <ayush22@navgurukul.org>
2024-05-13 23:33:07 +0530

    Adding Functionality for Making Operations

--- src/Components/Keys.js
+++ src/Components/Keys.js
@@ -7,47 +7,48 @@ function Keys() {
   function calculate(value) {
     if (value === "AC") {
       setValue("");
-      setResult('')
-    }
-    else if (getValue == "Del" ) {
-      setValue.slice(0,-1)
-      setResult(' ')
-    }
-    
-    else {
-      setValue (getValue + value)
+      setResult("");
+    } else if (value === "Del") {
+      setValue(getValue.slice(0, -1));
+      setResult(" ");
+    } else if (value === "=") {
+      setValue(eval(getValue));
+    } else {
+      setValue(getValue + value);
     }
   }
-
   return (
     <>
       <div className="calculator-Body">
         <div className="display">
-          <input type="text" placeholder="Enter The number" value={getValue} readOnly />
+          <input
+            type="text"
+            placeholder="Enter The number"
+            value={getValue}
+            readOnly
+          />
           <input type="text" value={result} readOnly />
         </div>
         <div className="numbers">
-          <button onClick={()=>calculate('AC')} >AC</button>
-          <button onClick={()=>calculate('Del')} >Del</button>
-          <button onClick={()=>calculate('%')} >%</button>
-          <button onClick={()=>calculate('/')} >/</button>
-          <button onClick={()=>calculate('7')} >7</button>
-          <button onClick={()=>calculate('8')} >8</button>
-          <button onClick={()=>calculate('9')} >9</button>
-          <button onClick={()=>calculate('x')} >X</button>
-          <button onClick={()=>calculate('4')} >4</button>
-          <button onClick={()=>calculate('5')} >5</button>
-          <button onClick={()=>calculate('6')} >6</button>
-          <button onClick={()=>calculate('-')} >-</button>
-          <button onClick={()=>calculate('1')} >1</button>
-          <button onClick={()=>calculate('2')} >2</button>
-          <button onClick={()=>calculate('3')} >3</button>  
-          <button onClick={()=>calculate('=')} >=</button>  
-          <button onClick={()=>calculate('+')} >+</button>
-          <button onClick={()=>calculate('0')} >0</button>
-          <button onClick={()=>calculate('.')} >.</button>
-          
-          
+          <button onClick={() => calculate("AC")}>AC</button>
+          <button onClick={() => calculate("Del")}>Del</button>
+          <button onClick={() => calculate("%")}>%</button>
+          <button onClick={() => calculate("/")}>/</button>
+          <button onClick={() => calculate("7")}>7</button>
+          <button onClick={() => calculate("8")}>8</button>
+          <button onClick={() => calculate("9")}>9</button>
+          <button onClick={() => calculate("x")}>X</button>
+          <button onClick={() => calculate("4")}>4</button>
+          <button onClick={() => calculate("5")}>5</button>
+          <button onClick={() => calculate("6")}>6</button>
+          <button onClick={() => calculate("-")}>-</button>
+          <button onClick={() => calculate("1")}>1</button>
+          <button onClick={() => calculate("2")}>2</button>
+          <button onClick={() => calculate("3")}>3</button>
+          <button onClick={() => calculate("=")}>=</button>
+          <button onClick={() => calculate("+")}>+</button>
+          <button onClick={() => calculate("0")}>0</button>
+          <button onClick={() => calculate(".")}>.</button>
         </div>
       </div>
     </>
