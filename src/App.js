f5d199a84bd635f237149a0bb7fbf8bf2f9a40ee
Ayush Sharma <ayush22@navgurukul.org>
2024-05-14 16:09:31 +0530

    Import Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Keys from './Components/Keys';
+import './App.css'
+
 
 function App() {
   return (
-    
+    <div className='main'>
+      <Keys/>
+    </div>
   )
 }
 
